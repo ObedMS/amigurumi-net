8fc086e5bf6ce627e2da6f81745aaa748868aa0c
Obed Moreno <ic_obedms@outlook.com>
2025-05-29 22:41:04 -0600

    Primer Commit

---
+++ src/data/productos.js
@@ -0,0 +1,15 @@
+export const productos = [
+    {
+      nombre: "Conejito Rosa",
+      imagen: "/img/conejito.jpg",
+      descripcion: "Amigurumi tejido con estambre suave y mucho amor.",
+      precio: 180
+    },
+    {
+      nombre: "Pulpo Morado",
+      imagen: "/img/pulpo.jpg",
+      descripcion: "Ideal para bebés o decoración tierna.",
+      precio: 220
+    }
+    // puedes agregar más
+  ];
